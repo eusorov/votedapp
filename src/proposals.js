92ed775411502909afd910b4ea355d7bd1d46fbf
evgeny <evgeny.usorov@lition.de>
2021-04-11 03:20:28 +0200

    proposals as extra file with desc

--- src/proposals.js
+++ src/proposals.js
@@ -1,6 +1,52 @@
 export default [
     { id: 0, name: "Choose proposal ..." },
-    { id: 1, name: "Capital X Fund" },
-    { id: 2, name: "NFTstreamz.tv" },
-    { id: 3, name: "DEFI + Contract NFT" },
+    { id: 1, name: "Capital X Fund", description: `Transfer the Lition ownership to the nodes including the treasury through a multisignature wallet and create the lition dao.   Also transfer the energy contract with lition energie. Redeploy the nodes on a cosmos sdk based sidechain (support for >hundred nodes through delegated pos and used by binance chain, luna, etc) Develop an additional defi usecase 
+    where tokens locked in nodes can be used as collateral for loans/synths while 
+    earning stable rewards due to energy as a fairly consistent segment.`},
+    { id: 2, name: "NFTstreamz.tv",  description: `
+    NFTstreamz.tv is a new project based in Manchester, UK. The concept is to give new avenue streams to streamers and viewers. <p/>
+    Users can stake their Lition/NFTS tokens to obtain NFT's that are used to post EMOJI/MEMES into chat channels. 
+    These are the only images users can post into the channel. Users can freely trade these NFT's on the market with a % of the sales going to the dev team. 
+    <p/>Side chains will be utilised to enable streamers to create their own NFT's and profit from the resale value on them. 
+    
+    <p/>The team consists of 4 people. Myself, a CTO, Head of Design & Head of Marketing. The funding will be used obtain more developers to steam roll the project forward and a portion to add liquidity to Uniswap. Locked and unlocked tokens will be portioned out into different pools should this idea gain traction. The project is fairly new and is in early stages. This transition will enable us to speed up the pipelines and enable the community too have a new lease of life on their investment. 
+    
+    <p/>We recognise the strong community here and will build a new, more rewarding brand.
+    ` },
+    { id: 3, name: "DEFI + Contract NFT",  description: `Our team suggests using our developments to create a new product
+    together with the Lition community.<br />
+    The main features of the project are: multi-blockchain DeFi, using the
+    advantages of scaling, speed and low cost of transactions of the new
+    blockchains, while maintaining the popularity of Ethereum.<br />
+    <p />
+    We will also build a new direction of using validated contracts of
+    real entities in NFT for trading them as collateral in DeFi, where
+    companies like Lition can wrap their energy contracts as NFT and make
+    them tradeable.
+    <p />
+    The members of the project are Anton Erashov, an economist currently
+    defending Ph.D in cryptocurrencies, previously taking part in various
+    crypto projects (https://www.linkedin.com/in/antonio-erashow/), Evgeny
+    Usorov, Software Engineer with many years of experience in Java /
+    JavaScript technologies and with experience in crypto and blockchain.
+    (https://www.linkedin.com/in/evgeny-usorov-344698199/) and other
+    developers.
+    <p />
+    After the launch of the project, which we are planning about 6 months,
+    Lition tokens will be exchanged for new DeFiCo tokens with 1 : 1. At
+    the same time, the initial fixed price of tokens is planned to be $ 1
+    with a restriction on price changes until the project reaches the 2.0
+    working stage and with a gradual transition to floating pricing. In
+    our version, for LIT holders, it will be important to support us in
+    voting and then track the news of the project's implementation. Later
+    through a smart contract you will be able to automatically replace
+    your LITs with DeFiCo tokens on the announced date (closer to the
+    public launch of the project).
+
+    <p />
+    It’s planned to open the office in Berlin closer to the technical
+    launch of the project and having a legal status: Limited company
+    (GmbH). There will be also first planned validations of NFT contracts
+    for launching in DeFi trading, with the subsequent development of a
+    network of offices throughout Europe and the world.` }
   ]
